c6d9704839209b40aa2654bb564b48bbb6bc0f25
dkanivets <sparrowufr@gmail.com>
2024-12-17 19:10:26 +0200

    -initital commit

---
+++ Find Your Pet/Find_Your_PetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_Your_PetApp.swift
+//  Find Your Pet
+//
+//  Created by Dmitry Kanivets on 17.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Find_Your_PetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
